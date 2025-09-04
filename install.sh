3ac767bccd204005f4aa269f84394bb59889ebb9
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2025-09-05 00:47:30 +0300

    fix install script

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env sh
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=true
+set -eaux
+
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=true -DBUILD_TESTING=false
 cmake --build build
 sudo cmake --install build
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=false
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=false -DBUILD_TESTING=false
 cmake --build build
 sudo cmake --install build
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=true
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=true -DBUILD_TESTING=false
 cmake --build build
 sudo cmake --install build
 
-cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=false
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=false -DBUILD_TESTING=false
 cmake --build build
 sudo cmake --install build
