bf5c27ee93a51179879c74e89b65cef27116df82
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2025-09-14 13:49:47 +0300

    gnuplot example

--- examples/gnuplot/main.c
+++ examples/gnuplot/main.c
@@ -5,9 +5,9 @@
 #include <unistd.h>
 
 #include "../../src/range.h"
+#include "../../src/format.h"
+#include "../../src/channel.h"
 #include "../../src/read.h"
-#include "../../src/filter.h"
-#include "../../src/csv.h"
 
 #ifndef PROGRAM_NAME
 #   define PROGRAM_NAME "hantek_drc_gnuplot"
@@ -54,6 +54,57 @@ void hit_enter(void) {
     } while (ch != '\n' && ch != EOF);
 }
 
+typedef struct hantek_drc_gnuplot_params {
+    FILE* pipe;
+    hantek_drc_format_handler format;
+} hantek_drc_gnuplot_params;
+
+bool hantek_drc_gnuplot_frame(void* params_any, const hantek_drc_channel* channel, const int16_t* frame) {
+    hantek_drc_gnuplot_params* params = (hantek_drc_gnuplot_params*) params_any;
+    for (size_t i = 0; i < channel->info->buffer_length; ++i) {
+        fprintf(params->pipe, "%zu,", channel->number);
+        hantek_drc_data_print(params->pipe, 
+            hantek_drc_format_type(&params->format, channel), 
+            hantek_drc_format_data(&params->format, channel, frame[i])
+        );
+        fputc('\n', params->pipe);
+    }
+    fputs("e\n", params->pipe);
+    return true;
+}
+
+bool hantek_drc_gnuplot_frame_prepare(void* params_any, const hantek_drc_info* info) {
+    hantek_drc_gnuplot_params* params = (hantek_drc_gnuplot_params*) params_any;
+    fprintf(params->pipe, "set title \"Frame %zu\"\n", info->frame_count);
+    for (size_t i = 0; i < info->channel_count; ++i) {
+        if (i == 0) {
+            fprintf(params->pipe, "plot \"-\"");
+        } else {
+            fprintf(params->pipe, ",\"\"");
+        }
+        fprintf(params->pipe, " with lines title \"CH%zu\"", (i + 1));
+    }
+    fputc('\n', params->pipe);
+    return true;
+}
+
+bool hantek_drc_gnuplot_frame_finish(void* params_any, const hantek_drc_info* info) {
+    (void)info;
+    hantek_drc_gnuplot_params* params = (hantek_drc_gnuplot_params*) params_any;
+    fflush(params->pipe);
+    hit_enter();
+    return true;
+}
+
+hantek_drc_frame_handler hantek_drc_gnuplot(hantek_drc_gnuplot_params* params) {
+    return (hantek_drc_frame_handler) {
+        .params = &params,
+        .on_frame = &hantek_drc_gnuplot_frame,
+        .on_frame_prepare = &hantek_drc_gnuplot_frame_prepare,
+        .on_frame_finish = &hantek_drc_gnuplot_frame_finish,
+    };
+}
+
 int main(int argc, char *argv[]) {
     (void)argv;
     if (argc < 2) {
@@ -70,10 +121,6 @@ int main(int argc, char *argv[]) {
     print_range(&frame_range);
     printf(")\n");
 
-    // hantek_drc_range_filter_params range_filter = {
-    //     .
-    // };
-
 
     FILE* gnuplot = popen("gnuplot", "w");
     if (gnuplot == NULL) {
@@ -81,13 +128,20 @@ int main(int argc, char *argv[]) {
     }
     fprintf(gnuplot, "set grid\n");
 
-    fprintf(gnuplot, "plot sin(x)\n");
-    fflush(gnuplot);
-    hit_enter();
-
-    fprintf(gnuplot, "plot cos(x)\n");
-    fflush(gnuplot);
-    hit_enter();
+    hantek_drc_data_format_params format = hantek_drc_data_format_volts(HANTEK_DRC_DATA_TYPE_F32);
+    hantek_drc_gnuplot_params params = {
+        .pipe = gnuplot,
+        .format = hantek_drc_data_format(&format)
+    };
+    hantek_drc_info info = {
+        .caps = hantek_drc_6254bd(),
+        .handler = hantek_drc_gnuplot(&params)
+    };
+    if (!hantek_drc_read_file(input_path, &info)) {
+        fprintf(stderr, "Can't read DRC file \"%s\"\n", input_path);
+        return -1;
+    }
+    hantek_drc_info_free(&info);
 
     pclose(gnuplot);
     return 0;
