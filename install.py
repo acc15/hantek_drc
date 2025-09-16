1cd8f51b0e8c082c9b0970201274bc89dde26c12
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2025-09-16 12:44:19 +0300

    install script

--- install.py
+++ install.py
@@ -2,16 +2,20 @@
 
 import itertools
 import os
+import sys
 
 variants = [
     { "shared": "-DBUILD_SHARED_LIBS=ON", "static": "-DBUILD_SHARED_LIBS=OFF" },
     { "debug": "-DCMAKE_BUILD_TYPE=Debug", "release": "-DCMAKE_BUILD_TYPE=Release" }
 ]
+
 for keys in itertools.product(*variants):
     options = " ".join(m[keys[i]] for i, m in enumerate(variants))
     build_dir = f"build/{"_".join(keys)}"
-    os.system( 
+    cmd = ( 
         f"cmake -S . -B {build_dir} -DBUILD_TESTING=OFF {options} && "
         f"cmake --build {build_dir} && "
-        f"{"sudo " if os.name == 'posix' else ""}cmake --install {build_dir}"
+        f"{"sudo " if os.name == "posix" else ""}cmake --install {build_dir}"
     )
+    if os.system(cmd) != 0:
+        sys.exit(-1)
