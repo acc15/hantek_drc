2c66c4088786a216aa0a39e7ec24f373ffd095e6
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2025-09-16 12:38:42 +0300

    python install script

---
+++ install.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import itertools
+import os
+
+variants = [
+    { "shared": "-DBUILD_SHARED_LIBS=ON", "static": "-DBUILD_SHARED_LIBS=OFF" },
+    { "debug": "-DCMAKE_BUILD_TYPE=Debug", "release": "-DCMAKE_BUILD_TYPE=Release" }
+]
+for keys in itertools.product(*variants):
+    options = " ".join(m[keys[i]] for i, m in enumerate(variants))
+    build_dir = f"build/{"_".join(keys)}"
+    os.system( 
+        f"cmake -S . -B {build_dir} -DBUILD_TESTING=OFF {options} && "
+        f"cmake --build {build_dir} && "
+        f"{"sudo " if os.name == 'posix' else ""}cmake --install {build_dir}"
+    )
